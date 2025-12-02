d621231f17ccf85e0cb96af40b0ebb987fe9a5a4
HTS-AI <hyperthinksystems9@gmail.com>
2025-12-02 17:27:59 +0530

    Change app run configuration for host and port

--- boilerapp/app.py
+++ boilerapp/app.py
@@ -109,6 +109,7 @@ def predict_h_dropdown():
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
 
